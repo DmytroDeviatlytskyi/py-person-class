56a41898a417bf2e84cd67ec1ef8f6da10a4832c
Dmytro Deviatlytskyi <videv1410@gmail.com>
2025-02-11 17:04:10 +0100

    add check: is wife or husband in Person.people

--- app/main.py
+++ app/main.py
@@ -10,11 +10,14 @@ class Person:
 def create_person_list(people: list) -> list:
     result = []
     for person in people:
+        Person(person["name"], person["age"])
         result.append(Person(person["name"], person["age"]))
     for person in people:
         name_of_person = Person.people[person["name"]]
-        if "wife" in person and person["wife"]:
+        if "wife" in person and person["wife"]\
+                and person["wife"] in Person.people:
             name_of_person.wife = Person.people[person["wife"]]
-        if "husband" in person and person["husband"]:
+        if "husband" in person and person["husband"]\
+                and person["husband"] in Person.people:
             name_of_person.husband = Person.people[person["husband"]]
     return result
